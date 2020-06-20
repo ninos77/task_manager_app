204b5bb4c095ebcab2c23858da2e33b09c4849dd
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-06-20 19:56:25 +0000

    fix a issue on  update function

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def edit_task(task_id):
                            categories=all_categories)
 
 
-@app.route("update_task/<task_id>", methods=["POST"])
+@app.route("/update_task/<task_id>", methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
     tasks.update({"_id": ObjectId(task_id)},
